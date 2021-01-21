b84ec00488de462d1a637002cfd775b3ce79a7d1
Rmiverson <Rmiverson99@gmail.com>
2021-01-21 15:46:17 -0800

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() + 1)
+
+let mod = 8 / 2
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,17,19,20)
